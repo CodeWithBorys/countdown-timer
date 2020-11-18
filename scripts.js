527b0135e85c1c0240e61446c1d6c4c8c93dd02f
Borys <bogori92@gmail.com>
2020-11-18 21:17:41 +0100

    display timer and end time

--- scripts.js
+++ scripts.js
@@ -1,9 +1,12 @@
 let countdown;
+const timeDisplay = document.querySelector('.timer__display-time-left');
+const endTime = document.querySelector('.timer__display-end-time');
 
 function timer(seconds) {
   const now = Date.now();
   const then = now + seconds * 1000;
   displayTimeLeft(seconds);
+  displayEndTime(then);
 
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now())/1000);
@@ -20,4 +23,14 @@ function timer(seconds) {
 function displayTimeLeft(seconds) {
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
+  const time = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+  document.title = time;
+  timeDisplay.textContent = time;
+}
+
+function displayEndTime(timestamp) {
+  const end = new Date(timestamp);
+  const hour = end.getHours();
+  const minutes = end.getMinutes();
+  endTime.textContent = `timer ends at ${hour}:${minutes}`
 }
